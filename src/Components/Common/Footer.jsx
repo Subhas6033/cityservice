b6a7582643ce9cc1cc42315bca20539deb0dd772
Subhas <goalkeepersubhas07@gmail.com>
2025-08-16 23:37:58 +0530

    added footer links

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom"
+
 
 const Footer = () => {
+
+  const footerItems = [
+    {
+      name: "Support",
+      navUrl : "/support"
+    }, {
+      name: "About us",
+      navUrl : "/about"
+    }, {
+      name: "Contact us",
+      navUrl : "/contact"
+    }, {
+      name: "Carrers",
+      navUrl : "/carers"
+    }, {
+      name: "Payments",
+      navUrl : "/payment"
+    }
+  ]
+
+  const navigate = useNavigate()
   return (
     <footer>
-      This is my footer
+      <ul>
+        {
+          footerItems.map((footer) => (
+            <li key={footer.name}>
+              <button
+              onClick={() => navigate(footer.navUrl)}
+              >
+                {
+                  footer.name
+                }
+              </button>
+            </li>
+          ))
+        }
+      </ul>
     </footer>
   )
 }
