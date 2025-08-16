b4a0bbcb67ca2ee5919a773f12618951da209f68
Subhas <goalkeepersubhas07@gmail.com>
2025-08-16 23:30:43 +0530

    added navitems

--- src/Components/Common/Nav.jsx
+++ src/Components/Common/Nav.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
+  const navItems = [
+    {
+      name: "About us",
+      navUrl: "/about",
+    },
+    {
+      name: "Services",
+      navUrl: "/service",
+    },
+    {
+      name: "Login",
+      navUrl: "/login",
+    },
+    {
+      name: "Contact us",
+      navUrl: "/contact",
+    },
+  ];
+
+  const navigate = useNavigate();
+
   return (
     <nav>
-      This is my Navbar
+      <ul>
+        {navItems.map((nav) => (
+          <li key={nav.name}>
+            <button onClick={() => navigate(nav.navUrl)}>{nav.name}</button>
+          </li>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
