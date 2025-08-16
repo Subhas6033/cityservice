02f6b18b552e30f1a48e1e45655d20fdb00d8e29
Subhas <goalkeepersubhas07@gmail.com>
2025-08-16 23:42:35 +0530

    Update layout

--- src/Components/Common/Layout.jsx
+++ src/Components/Common/Layout.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Layout = ({className = "", children, ...props}) => {
+const Layout = ({ className = "", children, ...props }) => {
   return (
-    <div className={`${className}`} {...props}>
-      {children}
+    <div
+      className={`min-h-screen flex flex-col bg-gray-50 text-gray-800 ${className}`}
+      {...props}
+    >
+      {/* Main Content Area */}
+      <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        {children}
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
